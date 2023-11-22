36e168ac08123b4f93f384be13bb0381353005c4
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-11-22 10:08:51 -0300

    refactor: update Home page layout and styling

--- src/app/(authentication)/page.tsx
+++ src/app/(authentication)/page.tsx
@@ -1,59 +1,47 @@
 import Image from "next/image";
-import Link from "next/link";
 import { UserAuthForm } from "./components/user-auth-form";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div className="container relative h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col dark:border-r lg:flex">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <Image
-            src="/banner-authentication.jpg"
-            alt="banner authentication"
             width={0}
             height={0}
             sizes="100vw"
-            className="h-full w-full"
+            className="mx-auto h-20 w-auto"
+            src="/logo.png"
+            alt="logo"
           />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-primary">
+            Entre com sua conta
+          </h2>
+          <p className="text-center text-sm text-muted">
+            Digite seu e-mail abaixo para poder entrar
+          </p>
         </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-3 text-center">
-              <Image
-                src="/logo.png"
-                alt="logo"
-                width={0}
-                height={0}
-                sizes="100vw"
-                className="m-auto block h-[60px] w-[150px]"
-              />
 
-              <h1 className="text-2xl font-semibold tracking-tight text-primary">
-                Criar uma conta
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Digite seu e-mail abaixo para criar sua conta
-              </p>
-            </div>
-            <UserAuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              Ao clicar em continuar, você concorda com nossos{" "}
-              <Link
-                href="#"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Termos de Serviço
-              </Link>{" "}
-              e{" "}
-              <Link
-                href="#"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Política de Privacidade.
-              </Link>
-              .
-            </p>
-          </div>
+        <div className="mt-10 space-y-6 sm:mx-auto sm:w-full sm:max-w-sm">
+          <UserAuthForm />
+          <p className="px-8 text-center text-sm text-muted">
+            Ao clicar em continuar, você concorda com nossos{" "}
+            <Link
+              href="/"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Termos de Serviço
+            </Link>{" "}
+            e{" "}
+            <Link
+              href="/"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Política de Privacidade.
+            </Link>
+            .
+          </p>
         </div>
       </div>
     </>
